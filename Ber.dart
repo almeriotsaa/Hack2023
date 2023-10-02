f7d3f0a320b1cc7e96d727dd8cc3d9cbf802c190
Almerio Tsany <89116363+almeriotsaa@users.noreply.github.com>
2023-10-02 15:26:05 +0700

    Create Ber.dart

---
+++ Ber.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  AppConstants._();
+  static const String key = 'RcGX7p3XwP';
+  static const String jwt = 'fCgFRCr72J';
+  static const String salt = 'PpPCVq2H7c';
+  static const int dbVersion = 44;
+  static const String dbName = 'ptnw.db';
+  static const String q = 'H42uS4lAy';
+  static const String appVersion = '1.4.4';
+  static const String copyright = '2023';
+}
